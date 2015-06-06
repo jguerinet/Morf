69921fe83a181bd70b7f64201bba89b439cdd68b
Julien Guerinet <julien@guerinet.com>
2015-06-06 00:04:37 -0400

    Overrode all of the methods in EditTextFormItem.

--- library/src/main/java/com/guerinet/formgenerator/EditTextFormItem.java
+++ library/src/main/java/com/guerinet/formgenerator/EditTextFormItem.java
@@ -16,7 +16,13 @@
 
 package com.guerinet.formgenerator;
 
+import android.graphics.Typeface;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DimenRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -66,6 +72,299 @@ public class EditTextFormItem extends TextViewFormItem {
 	public EditTextFormItem inputType(int type){
 		mEditText.setInputType(type);
 		return this;
+	}
+
+	/**
+	 * Sets the {@link Button} hint
+	 *
+	 * @param hint The hint
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem hint(String hint){
+		return (EditTextFormItem)super.hint(hint);
+	}
+
+	/**
+	 * Sets the {@link Button} hint
+	 *
+	 * @param stringId The String Id
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem hint(@StringRes int stringId){
+		return (EditTextFormItem)super.hint(stringId);
+	}
+
+	/**
+	 * Sets the {@link Button} text color
+	 *
+	 * @param colorId   The resource Id
+	 * @param stateList True if the color is a state list, false if it's a solid color
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem textColor(int colorId, boolean stateList){
+		return (EditTextFormItem)super.textColor(colorId, stateList);
+	}
+
+	/**
+	 * Sets the {@link Button} text size
+	 *
+	 * @param dimenId The dimension Id
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem textSizeDimen(@DimenRes int dimenId){
+		return (EditTextFormItem)super.textSizeDimen(dimenId);
+	}
+
+	/**
+	 * Sets the {@link Button} text size
+	 *
+	 * @param textSize The text size, in pixels
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem textSize(int textSize){
+		return (EditTextFormItem)super.textSize(textSize);
+	}
+
+	/**
+	 * Sets the {@link Button} padding
+	 *
+	 * @param left   The left padding, in pixels
+	 * @param top    The top padding, in pixels
+	 * @param right  The right padding, in pixels
+	 * @param bottom The bottom padding, in pixels
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem padding(int left, int top, int right, int bottom){
+		return (EditTextFormItem)super.padding(left, top, right, bottom);
+	}
+
+	/**
+	 * Sets the {@link Button} padding
+	 *
+	 * @param padding The padding to use for all sides, in pixels
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem padding(int padding){
+		return (EditTextFormItem)super.padding(padding);
+	}
+
+	/**
+	 * Sets the {@link Button} padding
+	 *
+	 * @param leftId   The dimension Id for the left padding
+	 * @param topId    The dimension Id for the top padding
+	 * @param rightId  The dimension Id for the right padding
+	 * @param bottomId The dimension Id for the bottom padding
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem paddingDimen(@DimenRes int leftId, @DimenRes int topId,
+			@DimenRes int rightId, @DimenRes int bottomId){
+		return (EditTextFormItem)super.paddingDimen(leftId, topId, rightId, bottomId);
+	}
+
+	/**
+	 * Sets the {@link Button} padding
+	 *
+	 * @param dimenId The dimension Id to use for all sides
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem paddingDimen(@DimenRes int dimenId){
+		return (EditTextFormItem)super.paddingDimen(dimenId);
+	}
+
+	/**
+	 * Sets the {@link Button} {@link Typeface}
+	 *
+	 * @param typeface The {@link Typeface}
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem typeface(Typeface typeface){
+		return (EditTextFormItem)super.typeface(typeface);
+	}
+
+	/**
+	 * Sets the {@link Button} {@link Typeface} and style
+	 *
+	 * @param typeface The {@link Typeface}
+	 * @param style    The style
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem style(Typeface typeface, int style){
+		return (EditTextFormItem)super.style(typeface, style);
+	}
+
+	/**
+	 * Sets the {@link Button} style. Note: if you are using a custom {@link Typeface},
+	 *  use {@link #style(Typeface, int)}
+	 *
+	 * @param style The style
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem style(int style){
+		return (EditTextFormItem) super.style(style);
+	}
+
+	/**
+	 * Sets the left icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @param visible True if the icon should be visible, false otherwise
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem leftIcon(@DrawableRes int iconId, boolean visible){
+		return (EditTextFormItem) super.leftIcon(iconId, visible);
+	}
+
+	/**
+	 * Sets the top icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @param visible True if the icon should be visible, false otherwise
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem topIcon(@DrawableRes int iconId, boolean visible){
+		return (EditTextFormItem) super.topIcon(iconId, visible);
+	}
+
+	/**
+	 * Sets the right icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @param visible True if the icon should be visible, false otherwise
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem rightIcon(@DrawableRes int iconId, boolean visible){
+		return (EditTextFormItem) super.rightIcon(iconId, visible);
+	}
+
+	/**
+	 * Sets the bottom icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @param visible True if the icon should be visible, false otherwise
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem bottomIcon(@DrawableRes int iconId, boolean visible){
+		return (EditTextFormItem) super.bottomIcon(iconId, visible);
+	}
+
+	/**
+	 * Sets the left icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem leftIcon(@DrawableRes int iconId){
+		return (EditTextFormItem) super.leftIcon(iconId);
+	}
+
+	/**
+	 * Sets the top icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem topIcon(@DrawableRes int iconId){
+		return (EditTextFormItem) super.topIcon(iconId);
+	}
+
+	/**
+	 * Sets the right icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem rightIcon(@DrawableRes int iconId){
+		return (EditTextFormItem) super.rightIcon(iconId);
+	}
+
+	/**
+	 * Sets the bottom icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem bottomIcon(@DrawableRes int iconId){
+		return (EditTextFormItem) super.bottomIcon(iconId);
+	}
+
+	/**
+	 * Sets the left icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @param colorId The color Id
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem leftIcon(@DrawableRes int iconId, @ColorRes int colorId){
+		return (EditTextFormItem) super.leftIcon(iconId, colorId);
+	}
+
+	/**
+	 * Sets the top icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @param colorId The color Id
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem topIcon(@DrawableRes int iconId, @ColorRes int colorId){
+		return (EditTextFormItem) super.topIcon(iconId, colorId);
+	}
+
+	/**
+	 * Sets the right icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @param colorId The color Id
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem rightIcon(@DrawableRes int iconId, @ColorRes int colorId){
+		return (EditTextFormItem) super.rightIcon(iconId, colorId);
+	}
+
+	/**
+	 * Sets the bottom icon
+	 *
+	 * @param iconId  The icon resource Id
+	 * @param colorId The color Id
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem bottomIcon(@DrawableRes int iconId, @ColorRes int colorId){
+		return (EditTextFormItem) super.bottomIcon(iconId, colorId);
+	}
+
+	/**
+	 * Sets the {@link View.OnClickListener}
+	 *
+	 * @param listener The {@link View.OnClickListener}
+	 * @return The {@link EditTextFormItem} instance
+	 */
+	@Override
+	public EditTextFormItem onClick(View.OnClickListener listener){
+		return (EditTextFormItem) super.onClick(listener);
 	}
 
 	/**
