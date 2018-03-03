529b61fc4a66e066287bc807f8748e9c02686346
Julien Guerinet <julien@guerinet.com>
2018-03-03 17:02:19 -0500

    Added the Layout util variables.

---
+++ library/src/main/kotlin/com/guerinet/fg/util/Layout.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015-2018 Julien Guerinet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.guerinet.fg.util
+
+import android.view.ViewGroup
+
+/**
+ * Convenience layout variables for setting the height and width
+ * @author Julien Guerinet
+ * @since 5.0.0
+ */
+object Layout {
+
+    const val MATCH_PARENT = ViewGroup.LayoutParams.MATCH_PARENT
+
+    const val WRAP_CONTENT = ViewGroup.LayoutParams.WRAP_CONTENT
+}
